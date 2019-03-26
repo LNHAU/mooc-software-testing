1daa69433de6d566e28d23db0c9f24506535b3a7
LNHAU <37344872+LNHAU@users.noreply.github.com>
2019-03-26 12:14:36 +0100

    Correct CountLetters class and add 1 test
    1 line "if(last == 'x' || last == 's')" changed into : "if (last == 'r' || last == 's')"

---
+++ CountLettersTest.java
@@ -0,0 +1,26 @@
+package tudelft.countletters;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CountLettersTest {
+
+    @Test
+    public void multipleMatchingWords() {
+        int words = new CountLetters().count("cats|dogs");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void lastWordDoesNotMatch() {
+        int words = new CountLetters().count("cats|dog");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void lastWordDoesMatch() {
+        int words = new CountLetters().count("cat|her");
+        Assertions.assertEquals(1, words);
+    }
+
+}
